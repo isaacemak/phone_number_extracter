bb1973d2db7c3ed0e4c67e9b6a69eef8c5c0027e
isaacemak <isaacemak@gmail.com>
2015-09-20 23:37:44 +0800

    读取指定文件夹下所有.xls文件输出电话号码。

--- extracter.py
+++ extracter.py
@@ -1,48 +1,42 @@
-#-*- coding: utf8 -*-
-
-#廖彧的xls文件清洗器，从xls文件提取电话号码
+# coding: utf-8
 import xlrd
 import re
 import sys
+import getopt
+import os
 
-if len(sys.argv)!=2:
-	exit()
-
-pattern= re.compile(r'\d{8,}')
-
-fname = sys.argv[1]
-bk = xlrd.open_workbook(fname)
-shxrange = range(bk.nsheets)
-try:
-    sh = bk.sheet_by_name("Sheet1")
-except:
-    pass
-    #print "no sheet in %s named Sheet1" % fname
-#获取行数
-nrows = sh.nrows
-#获取列数
-ncols = sh.ncols
-#print "nrows %d, ncols %d" % (nrows,ncols)
-#获取第一行第一列数据 
-cell_value = sh.cell_value(1,1)
-#print cell_value
+def Usage():
+    print 'test.py usage:'
+    print '-h,--help: print help message.'
+    print '-v, --version: print script version'
 
 
-#获取各行数据
-for i in range(1,nrows):
-    row_data = sh.row_values(i)
-    
-    for s in row_data:
-    	try:
-    		#print
-    		#print(str(s))    		
-    		match= pattern.match(str(s))
-    		if match:
-    			print(match.group(0))
 
-    	except:
-    		pass
-    		#sys.stderr.write('ERROR\n');
-    		
-    	
+def get_phone_number_from_file(fname):
+	pattern= re.compile(r'\d{8,}')
+	bk= xlrd.open_workbook(fname)
+	sheets= bk.sheets()
+	for sh in sheets:
+		for i in range(0,sh.nrows):
+			temp= sh.row_values(i)	
+			ans= str(temp)
+			phone_numbers=	re.findall(pattern, ans)
+			for phone_number in phone_numbers:
+				print(phone_number)
+
+def get_phone_number_from_folder(foldername):
+	files= os.listdir(foldername)
+	for f in files:
+		get_phone_number_from_file(foldername+'/'+f)
+
+
+if __name__ == '__main__':
+	if len(sys.argv)!=2:
+		sys.stderr.write("ERROR: Please input the foldername name.\n")		
+		exit()
+#	get_phone_number_from_file(sys.argv[1])
+	get_phone_number_from_folder(sys.argv[1])	
+
+
+
 
